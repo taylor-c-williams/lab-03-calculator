19f08ab2bb5af75885ae183da2dc10ccbe3ebf86
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-06-03 14:10:56 -0700

    Added add UI and button listener

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // import functions
 
 // reference needed DOM elements
+const addXInput = document.getElementById('add-x-input');
+const addYInput = document.getElementById('add-x-input');
+const addButton = document.getElementById('add-button');
+const addResult = document.getElementById('add-result');
 
-
+console.log(addXInput, addYInput, addButton, addResult);
 // set event listeners 
+
+addButton.addEventListener('click', ()=> {
+  console.log('add button clicked')
+} )
+
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
